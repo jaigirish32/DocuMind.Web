ef16a8575f0b88180b6dd3aa703ec4e3bc8b02c2
jaigirish32 <jaigirish32@gmail.com>
2026-03-26 19:43:02 +0530

    fix: hardcode static ngrok URL as API base URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: 'https://flabbier-interspinal-neely.ngrok-free.dev',
 })
 
 export const uploadDocument = async (file) => {
